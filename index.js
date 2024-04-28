30b54f0a2965fce5a98471843a6dc06a88737951
Aryan Sanghvi <122285469+aryanksanghvi@users.noreply.github.com>
2024-04-28 23:39:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,107 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-// const bcrypt = require('bcrypt');
-
-// const app = express();
-// app.use(bodyParser.urlencoded({
-//   extended: false
-// }));
-
-// // Middleware
-// app.use(express.json());
-// app.use(express.urlencoded({
-//   extended: false
-// }));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // MongoDB Connection
-// const uri = "mongodb+srv://aryanksanghvi:AAWz5JSw6p962JVL@recipe.gmwfgw0.mongodb.net/credentials?retryWrites=true&w=majority&appName=Recipe";
-// mongoose.connect(uri)
-//   .then(() => console.log("Database Connected Successfully"))
-//   .catch(err => console.log("Database cannot be Connected", err));
-
-
-// // User Schema
-// const Loginschema = new mongoose.Schema({
-//   username: {
-//     type: String,
-//     required: true
-//   },
-//   password: {
-//     type: String,
-//     required: true
-//   }
-// });
-
-// const User = mongoose.model("users", Loginschema);
-
-// // Routes
-// const intial = path.join(__dirname, "public");
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(intial, "./login/login.html"));
-// });
-// app.get('/login', (req, res) => {
-//   res.sendFile(path.join(intial, "./login/login.html"));
-// });
-
-// app.get('/home', (req, res) => {
-//   res.sendFile(path.join(intial, "./home/home.html"));
-// });
-// app.post('/login', async (req, res) => {
-//   try {
-//     const {
-//       username,
-//       password
-//     } = req.body;
-//     const user = await User.findOne({
-//       username
-//     });
-//     if (!user) {
-//       return res.send('User not found');
-//     }
-//     const isPasswordValid = await bcrypt.compare(password, user.password);
-//     if (!isPasswordValid) {
-//       return res.send('Incorrect password');
-//     }
-//     res.redirect('/home'); // Or wherever you want on success
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Login failed due to an error');
-//   }
-// });
-
-// app.post('/signup', async (req, res) => {
-//   try {
-//     const {
-//       username,
-//       password
-//     } = req.body;
-//     const existingUser = await User.findOne({
-//       username
-//     });
-//     if (existingUser) {
-//       return res.send('User already exists');
-//     }
-//     const saltRounds = 10;
-//     const passwordHash = await bcrypt.hash(password, saltRounds);
-//     const newUser = new User({
-//       username,
-//       password: passwordHash
-//     });
-//     await newUser.save();
-//     console.log('User saved successfully!');
-//     res.redirect('/login'); // Redirect to login page after signup
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Signup failed due to an error');
-//   }
-// });
-
-// // Start the server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 const express = require('express');
 const path = require('path');
@@ -122,7 +18,7 @@ app.use(express.urlencoded({
 app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB Connection
-const uri = "mongodb+srv://aryanksanghvi:AAWz5JSw6p962JVL@recipe.gmwfgw0.mongodb.net/credentials?retryWrites=true&w=majority&appName=Recipe";
+const uri = "YOUR MONGO DB URL";
 mongoose.connect(uri)
   .then(() => console.log("Database Connected Successfully"))
   .catch(err => console.log("Database cannot be Connected", err));
